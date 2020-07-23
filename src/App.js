2f50a31bc1feeca0019a00a5f3988a9b85e21167
rai8 <hytonne@outlook.com>
2020-07-23 22:09:24 +0300

    components creation and reusing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Person from "./components/Person"
+
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Introduction to React</h1>
+      <div style={styles}>
+        <Person name="Rhytonne" age="24" />
+        <Person name="Bridgit" age="17" />
+      </div>
     </div>
-  );
+  )
+}
+const styles = {
+  display: "flex",
 }
 
-export default App;
+export default App
---
+++ src/components/Person.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const Person = (props) => {
+  return (
+    <div style={styles}>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+    </div>
+  )
+}
+const styles = {
+  border: "1px solid #eee",
+  width: "200px",
+  marginLeft: "10px",
+  boxShadow: "1px 1px 3px grey",
+  padding: "20px",
+  marginTop: "15px",
+}
+export default Person
